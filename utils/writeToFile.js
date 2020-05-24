54af6df8bb08c191b3e6d0befda35d9c63be24dc
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 16:41:10 +0100

    Created basic writeToFile() function

--- utils/writeToFile.js
+++ utils/writeToFile.js
@@ -2,16 +2,49 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 const printHeader = require('./printHeader');
+const generateMarkdown = require('./generateMarkdown');
 
+// Prompting for a project name and setting directory
 const projectSelect = async () => {
   let path = await inquirer.prompt({
-    name: 'changeDefault',
-    message: 'Enter output directory path or leave blank for default'
-      .magenta.bold,
-    default: '~/Documents/my-project/',
+    name: 'projectName',
+    message: 'Enter project name or leave blank for default.'.magenta.bold,
+    default: 'my-project',
   });
-  path = path.changeDefault + 'README.md';
-  return path
+  path = `./output/${path.projectName}`;
+  return path;
+};
+
+// Writing the new directory and file
+const writeToFile = async (data) => {
+  printHeader();
+  let path = await projectSelect();
+
+  try {
+    // Checking if file exists already
+    if (fs.existsSync(`${path}/README.md`)) {
+      let filenameConfirmed = false;
+      // While loop to repeatedly test input
+      while (!filenameConfirmed) {
+        printHeader();
+        const confirmation = await inquirer.prompt({
+          type: 'confirm',
+          name: 'overwrite',
+          message: `${path}/README.md already exists, do you wish to overwite?`,
+        });
+        filenameConfirmed = confirmation.overwrite;
+      }
+    } else {
+      fs.mkdirSync(path);
+    }
+    printHeader();
+    fs.writeFile(`${path}/README.md`, generateMarkdown(data), (err) => {
+      if (err) throw err;
+      console.log('saved');
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = writeToFile;
