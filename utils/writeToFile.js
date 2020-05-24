00bbcac00ee93a98487647b47e098d4b8bffdf1b
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 20:37:09 +0100

    Refactored

--- utils/writeToFile.js
+++ utils/writeToFile.js
@@ -5,51 +5,37 @@ const printHeader = require('./printHeader');
 const generateMarkdown = require('./generateMarkdown');
 const callGithub = require('./callGithub')
 
-// Prompting for a project name and setting directory
-const projectSelect = async () => {
-  let path = await inquirer.prompt({
-    name: 'projectName',
-    message: 'Enter project name or leave blank for default.'.magenta.bold,
-    default: 'my-project',
-  });
-  // Removing unwanted characters from filename and replacing with '-'
-  const projectName = path.projectName
-    .split(/(?:,| |\[|\]|\:|\;|\||\*|")+/)
-    .join('-');
-  return projectName;
-};
-
 // Writing the new directory and file
 const writeToFile = async (data) => {
   printHeader();
-  let projectName = await projectSelect();
+  let projectName = data.title.split(/(?:,| |\[|\]|\:|\;|\||\*|")+/).join('-');
   let path = `./output/${projectName}`;
-
+  
   try {
-    // Checking if file exists already
-    if (fs.existsSync(`${path}/README.md`)) {
-      let filenameConfirmed = false;
-      // While loop to repeatedly test input
-      while (!filenameConfirmed) {
-        printHeader();
-        const confirmation = await inquirer.prompt({
-          type: 'confirm',
-          name: 'overwrite',
-          message: `${path}/README.md already exists, do you wish to overwite?`,
-        });
-        if (!confirmation.overwrite) {
-          projectName = await projectSelect();
-          path = `./output/${projectName}`;
+      // Checking if file exists already
+      if (fs.existsSync(`${path}/README.md`)) {
+          let filenameConfirmed = false;
+          // While loop to repeatedly test input
+          while (!filenameConfirmed) {
+              printHeader();
+              const confirmation = await inquirer.prompt({
+                  type: 'confirm',
+                  name: 'overwrite',
+                  message: `${path}/README.md already exists, do you wish to overwite?`,
+                });
+                if (!confirmation.overwrite) {
+                    projectName = data.title.split(/(?:,| |\[|\]|\:|\;|\||\*|")+/).join('-');
+                    path = `./output/${projectName}`;
+                }
+                filenameConfirmed = confirmation.overwrite;
+            }
+        } else if (!fs.existsSync(path)) {
+            fs.mkdirSync(path);
         }
-        filenameConfirmed = confirmation.overwrite;
-      }
-    } else if (!fs.existsSync(path)) {
-      fs.mkdirSync(path);
-    }
-
-    // Calling github api
-    githubData = await callGithub(data.username, projectName);
-
+        
+        // Calling github api
+        githubData = await callGithub(data.username, projectName);
+        
     // Writing the output file
     printHeader();
     fs.writeFile(`${path}/README.md`, generateMarkdown(projectName, data, githubData), (err) => {
