12f2145583940c1ad624cc00ddc2a12776a05be4
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 18:58:14 +0100

    Created template readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,17 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
+<h1 align="center">${data.title}</div>
 
+<div align="center">
+  <img src="https://img.shields.io/npm/l/m?style=plastic" alt="License Badge" />
+  <img src="https://img.shields.io/github/issues/goldenaries658/Readme-generator" alt="Issues Badge"/>
+</div>
+<div align="center">
+  <img src="https://img.shields.io/github/package-json/dependency-version/goldenaries658/README-Generator/inquirer" alt="Inquirer Version Badge"/>
+  <img src="https://img.shields.io/github/package-json/dependency-version/goldenaries658/readme-generator/axios" alt="Axios Version Badge"  />
+  <img src="https://img.shields.io/github/package-json/dependency-version/goldenaries658/readme-generator/colors" alt="Colors Version Badge"  />
+  <img src="https://img.shields.io/github/package-json/dependency-version/goldenaries658/readme-generator/dotenv" alt="Dotenv Version Badge"  />
+</div>
 ## Description
 
 ${data.description}
