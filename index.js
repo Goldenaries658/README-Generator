8d5e5ead6a5ebc6976400b469e77e5b648d9a66e
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 13:14:51 +0100

    Added all required modules

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-const questions = [
+const fs = require('fs');
+const axios = require('axios')
+const inquirer = require('inquirer')
+const util = require('util')
+const colors = require('colors')
 
+const questions = [
+    
 ];
 
 const writeToFile = (fileName, data) => {
-    
+
 }
 
 const init = () => {
