e385f1f59f334db1d3873adf119aa64eb3c6a6b8
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 20:27:13 +0100

    Added handler for no email found

--- utils/callGithub.js
+++ utils/callGithub.js
@@ -1,9 +1,20 @@
 const axios = require('axios');
+const inquirer = require('inquirer');
 
 const callGithub = async (username, projectName) => {
   const queryURL = `https://api.github.com/users/${username}`;
   try {
     const response = await axios.get(queryURL);
+    // Checking if there is an email in the response and manually setting one if not
+    if (!response.email) {
+      console.log('No GitHub email found!'.red.bold);
+      const email = await inquirer.prompt({
+        type: 'confirm',
+        name: email,
+        message: 'Please enter email:'.magenta.bold,
+      });
+      response.email = email
+    }
     return response;
   } catch (err) {
     console.log(err);
