8dd3a05be5d75492a2b88069e245a929a6a77a50
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 17:44:27 +0100

    Added template README

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,44 @@ function generateMarkdown(data) {
   return `
 # ${data.title}
 
+## Description
+
+${data.description}
+
+## Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Licence
+
+${data.licence}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+${data.questions}
+
+
 `;
 }
 
