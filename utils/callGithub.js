b8ea5531dcee6828704c72d38abb7a179083f340
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-05-24 21:04:45 +0100

    Fixed api call

--- utils/callGithub.js
+++ utils/callGithub.js
@@ -1,10 +1,12 @@
 const axios = require('axios');
 const inquirer = require('inquirer');
 
-const callGithub = async (username, projectName) => {
+const callGithub = async (username) => {
   const queryURL = `https://api.github.com/users/${username}`;
+  console.log(queryURL.red)
   try {
     const response = await axios.get(queryURL);
+    console.log(response.data)
     // Checking if there is an email in the response and manually setting one if not
     if (!response.email) {
       console.log('No GitHub email found!'.red.bold);
@@ -13,7 +15,7 @@ const callGithub = async (username, projectName) => {
         name: email,
         message: 'Please enter email:'.magenta.bold,
       });
-      response.email = email
+      response.email = email.email
     }
     return response;
   } catch (err) {
--- utils/writeToFile.js
+++ utils/writeToFile.js
@@ -3,45 +3,56 @@ const fs = require('fs');
 
 const printHeader = require('./printHeader');
 const generateMarkdown = require('./generateMarkdown');
-const callGithub = require('./callGithub')
+const callGithub = require('./callGithub');
 
 // Writing the new directory and file
 const writeToFile = async (data) => {
   printHeader();
   let projectName = data.title.split(/(?:,| |\[|\]|\:|\;|\||\*|")+/).join('-');
   let path = `./output/${projectName}`;
-  
+
   try {
-      // Checking if file exists already
-      if (fs.existsSync(`${path}/README.md`)) {
-          let filenameConfirmed = false;
-          // While loop to repeatedly test input
-          while (!filenameConfirmed) {
-              printHeader();
-              const confirmation = await inquirer.prompt({
-                  type: 'confirm',
-                  name: 'overwrite',
-                  message: `${path}/README.md already exists, do you wish to overwite?`,
-                });
-                if (!confirmation.overwrite) {
-                    projectName = data.title.split(/(?:,| |\[|\]|\:|\;|\||\*|")+/).join('-');
-                    path = `./output/${projectName}`;
-                }
-                filenameConfirmed = confirmation.overwrite;
-            }
-        } else if (!fs.existsSync(path)) {
-            fs.mkdirSync(path);
+    // Checking if file exists already
+    if (fs.existsSync(`${path}/README.md`)) {
+      let filenameConfirmed = false;
+      // While loop to repeatedly test input
+      while (!filenameConfirmed) {
+        printHeader();
+        const confirmation = await inquirer.prompt({
+          type: 'confirm',
+          name: 'overwrite',
+          message: `${path}/README.md already exists, do you wish to overwite?`,
+        });
+        if (!confirmation.overwrite) {
+          projectName = data.title
+            .split(/(?:,| |\[|\]|\:|\;|\||\*|")+/)
+            .join('-');
+          path = `./output/${projectName}`;
         }
-        
-        // Calling github api
-        githubData = await callGithub(data.username, projectName);
-        
+        filenameConfirmed = confirmation.overwrite;
+      }
+    } else if (!fs.existsSync(path)) {
+      fs.mkdirSync(path);
+    }
+
+    // Calling github api
+    const githubData = await callGithub(data.github);
+    console.log(githubData);
+    const blah = await inquirer.prompt({
+      name: 'test',
+      message: 'blah',
+    });
+
     // Writing the output file
     printHeader();
-    fs.writeFile(`${path}/README.md`, generateMarkdown(projectName, data, githubData), (err) => {
-      if (err) throw err;
-      console.log('saved');
-    });
+    fs.writeFile(
+      `${path}/README.md`,
+      generateMarkdown(projectName, data, githubData),
+      (err) => {
+        if (err) throw err;
+        console.log('saved');
+      }
+    );
   } catch (error) {
     console.log(error);
   }
