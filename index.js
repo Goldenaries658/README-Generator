214a7354dd0885b43040786722b6a17f052777f5
Goldenaries658 <59972622+Goldenaries658@users.noreply.github.com>
2020-06-11 23:49:39 +0100

    Added input validation

--- index.js
+++ index.js
@@ -3,36 +3,44 @@ const colors = require('colors');
 
 const writeToFile = require('./utils/writeToFile');
 const printHeader = require('./utils/printHeader');
+const emptyInputCheck = require('./utils/inputValidators');
 
 // Array of question to ask user
 const questions = [
   {
     name: 'title',
     message: 'Enter project title:'.magenta.bold,
+    validate: emptyInputCheck,
   },
   {
     name: 'description',
     message: 'Enter project description:'.magenta.bold,
+    validate: emptyInputCheck,
   },
   {
     name: 'installation',
     message: 'Enter project installation instructions:'.magenta.bold,
+    validate: emptyInputCheck,
   },
   {
     name: 'usage',
     message: 'Provide instructions and examples for use:'.magenta.bold,
+    validate: emptyInputCheck,
   },
   {
     name: 'contributing',
     message: 'Enter in guidelines for contributing:'.magenta.bold,
+    default: 'Contributor Covenant',
   },
   {
     name: 'tests',
     message: 'Enter detail of any test packages in the project:'.magenta.bold,
+    validate: emptyInputCheck,
   },
   {
     name: 'github',
     message: 'Enter GitHub username:'.magenta.bold,
+    validate: emptyInputCheck,
   },
 ];
 
